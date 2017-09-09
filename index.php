930bc8319a4e5f4bcd4c2ac55d7dd1a513c09152
Ghazanfar Mir <GhazanfarMir@gmail.com>
2017-09-09 23:49:10 +0100

    removed commented statements

--- index.php
+++ index.php
@@ -31,26 +31,12 @@ try {
     print_r('Search Disqualified Officers: ' . $disqualified_officers->items[0]->title . PHP_EOL);
 
     // Companies resources
-     $company = $api->company('07086058')->get();
-     print_r('Search Company byNumber: ' . $company->company_name . PHP_EOL);
+    $company = $api->company('07086058')->get();
+    print_r('Search Company byNumber: ' . $company->company_name . PHP_EOL);
 
-     // Search Officers
-     $officers = $api->company('07086058')->officers();
-     print_r('Company Officers: ' . $officers->items[0]->name . PHP_EOL);
-
-     // Search Disqualified Officers
-     $disqualified = $api->company('07086058')->disqualified();
-     print_r('Search Disqualified Officers: ' . $disqualified->items[0]->title . PHP_EOL);
-
-     // company officers
-     print_r('Show Company officers: ' . $api->company()->with(['officers'])->find('07086058')->officers()->items[0]->name . PHP_EOL);
-
-     // registered address
-     $o = $api->company()->with(array('officers', 'charges', 'registered_office_address', 'filing_history'))->find('07086058');
-     print_r('Company registered address: ' . $o->registeredOfficeAddress()->address_line_1. PHP_EOL);
-
-     // filing history
-     print_r('Show Company Filing history: '.$o->filingHistory()->items[0]->description . PHP_EOL);*/
+    // Search Officers
+    $officers = $api->company('07086058')->officers();
+    print_r('Company Officers: ' . $officers->items[0]->name . PHP_EOL);
 
 } catch (Exception $e) {
     echo $e->getMessage();
