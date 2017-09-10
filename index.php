3c420dd7c51d62bd1831a54c136f66c4c6d30f88
Ghazanfar Mir <GhazanfarMir@gmail.com>
2017-09-10 14:29:57 +0100

    index file updated to reflect new changes

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ use GhazanfarMir\CompaniesHouse\Http\Client;
 
 include 'vendor/autoload.php';
 
+define('NEWLINE', php_sapi_name()==='cli'?PHP_EOL:'<br>');
+
 try {
 
     $base_uri = 'https://api.companieshouse.gov.uk/';
@@ -19,24 +21,24 @@ try {
 
     // search resources
     $all = $api->search()->all($company);
-    print_r('Search all: ' . $all->items[0]->title . PHP_EOL);
+    print_r('Search all: ' . $all->items[0]->title . NEWLINE);
 
     $companies = $api->search()->companies('Ebury Partners');
-    print_r('Search Companies: ' . $companies->items[0]->title . PHP_EOL);
+    print_r('Search Companies: ' . $companies->items[0]->title . NEWLINE);
 
     $officers = $api->search()->officers('Mir');
-    print_r('Search Officers: ' . $officers->items[0]->title . PHP_EOL);
+    print_r('Search Officers: ' . $officers->items[0]->title . NEWLINE);
 
     $disqualified_officers = $api->search()->disqualified_officers('Mir');
-    print_r('Search Disqualified Officers: ' . $disqualified_officers->items[0]->title . PHP_EOL);
+    print_r('Search Disqualified Officers: ' . $disqualified_officers->items[0]->title . NEWLINE);
 
     // Companies resources
     $company = $api->company('07086058')->get();
-    print_r('Search Company byNumber: ' . $company->company_name . PHP_EOL);
+    print_r('Search Company byNumber: ' . $company->company_name . NEWLINE);
 
     // Search Officers
     $officers = $api->company('07086058')->officers();
-    print_r('Company Officers: ' . $officers->items[0]->name . PHP_EOL);
+    print_r('Company Officers: ' . $officers->items[0]->name . NEWLINE);
 
 } catch (Exception $e) {
     echo $e->getMessage();
