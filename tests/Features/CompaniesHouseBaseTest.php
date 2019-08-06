d0695387a2e790940e9d26b558da8713497869a0
Ghazanfar Mir <GhazanfarMir@gmail.com>
2019-08-06 23:16:00 +0100

    Update return type in setup()
    Signed-off-by: Ghazanfar Mir <GhazanfarMir@gmail.com>

--- tests/Features/CompaniesHouseBaseTest.php
+++ tests/Features/CompaniesHouseBaseTest.php
@@ -36,7 +36,7 @@ abstract class CompaniesHouseBaseTest extends TestCase
     /**
      * @test
      */
-    public function setUp()
+    public function setUp(): void 
     {
         parent::setUp();
 
